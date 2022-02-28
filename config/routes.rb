62aed703202372ee91929f07e4eff6c3e39de09d
Shuhaha16 <jeff.shoemaker@kellogg.northwestern.edu>
2022-02-28 02:32:09 +0000

    added root to route and places list in html (temp)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   resources :places
   resources :posts
+
+  # Defines the root path route ("/")
+  root to: "places#index"
+
 end
