69af2d22a9611be085ecd511749bd9799b3172e8
Shuhaha16 <jeff.shoemaker@kellogg.northwestern.edu>
2022-02-28 03:02:06 +0000

    added new.html.erb for places, and added new and create actions to places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,3 +1,14 @@
 class PlacesController < ApplicationController
     
+    
+    
+    def new
+        @post = Post.new        
+    end
+
+    def create
+        @post = Post.new(params["posts"])
+        @post.save
+        redirect_to "/places"
+    end
 end
