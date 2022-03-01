93857491dacd9e25a2bd79b0841fd1aca8a48443
Shuhaha16 <jeff.shoemaker@kellogg.northwestern.edu>
2022-03-01 02:54:51 +0000

    oh boy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,14 @@
 class PlacesController < ApplicationController
     
-    
+    # <% places = ["Portugal","Norway", "England"] %>
     
     def new
         @place = Place.new        
     end
 
     def create
-        # @place = Place.new(params["places"])
-        # @place.save
+        @place = Place.new(params["places"])
+        @place.save
         redirect_to "/places"
     end
 end
