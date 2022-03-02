9572bdfff1673dac1b1c68fd60eab5cf59452e13
Shuhaha16 <jeff.shoemaker@kellogg.northwestern.edu>
2022-03-02 01:55:29 +0000

    # out post seeds, added return to places link

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,23 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+#TODO
+#-syntax of initial post creation
+#-how to reset place id upon destroy all
+#-what is t.date syntax (ex/ "02/02/2022"?)
+
 places = Place.destroy_all
 #Comment out if you don't want to restart post database each time
 
 places = Place.create([{ name: "Norway"}, {name: "Portugal"}, {name: "Hyde Park" }])
 #Initial seed of model
-posts = Post.create([
-    {title: "The beginning", description: "It was a dark and story night...", posted_on: 02/21/22, place_id = "14" },
-    {title: "The Middle", description: "a Twosday.", posted_on: 02/22/22, place_id = "14" },
-    {title: "The End", description: "and finally wednesday.", posted_on: 02/23/22, place_id = "14" }
-])
+# posts = Post.create([
+#     {title: "The beginning", description: "It was a dark and story night...", posted_on: "02/21/22", place_id = "14" }
+#     {title: "The Middle", description: "a Twosday.", posted_on: "02/22/22", place_id = "14" }
+#     {title: "The End", description: "and finally wednesday.", posted_on: "02/23/22", place_id = "14"} 
+# ])
   #connect place id to location? id might change with db:seed command
+
 
 # t.string :title
 # t.text :description
